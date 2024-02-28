52f2d124725034a154045211dafb618a9dff36d0
Krishna Varshney <commercekv2003@gmail.com>
2024-02-28 19:20:29 +0530

    add teh vercel json

--- server.js
+++ server.js
@@ -3,29 +3,29 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const itemModel = require('./models/item_model');
+const itemModel = require('./src/models/item_model');
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
 
 
 
-const mongoDbPath = 'mongodb+srv://commercekv2003:iphFdHXxDeM6lyfY@cluster123.mybud88.mongodb.net/items';
-mongoose.connect(mongoDbPath).then(function(){
+// const dbHost = process.env.mongoDbPath;
+const dbHost = 'mongodb+srv://commercekv2003:iphFdHXxDeM6lyfY@cluster123.mybud88.mongodb.net/items';
+mongoose.connect(dbHost).then(function(){
     //routes
     app.get('/', function (req, res) {
         const response = { message: "API is working" };
         res.json(response);
     });
-    const itemRouter = require('./routes/item_routes');
+    const itemRouter = require('./src/routes/item_routes');
     app.use('/items',itemRouter);
 });
 
 
-
-
-
 // on the request to root (localhost:3000/)
-app.listen(3000,function(req,res){
-    console.log("Server is running on port 3000");
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,function(req,res){
+    console.log("Server is running on port ",PORT);
 });
